35bd6fe9b1bc528c68beab0b149bb75657920577
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-04-02 02:06:47 +0600

    camera setup

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -5,14 +5,18 @@ import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk'
 import MeetingSetup from '@/components/MeetingSetup'
 import MeetingRoom from '@/components/MeetingRoom'
 import { useGetCallById } from '@/hooks/useGetCallById'
+import Loader from '@/components/Loader'
 
 const Meeting = ({ params: {id} }: {params: {id: string}}) => {
   const { user, isLoaded } = useUser()
   const [isSetupComplete, setIsSetupComplete] = useState(false)
   const {call, isCallLoading} = useGetCallById(id)
+
+  if(!isLoaded || isCallLoading) return <Loader />
+
   return (
     <main className='h-screen w-full'>
-      <StreamCall>
+      <StreamCall call={call}>
         <StreamTheme>
           {!isSetupComplete ? (
             <MeetingSetup />
--- src/components/MeetingSetup.tsx
+++ src/components/MeetingSetup.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+'use client'
+
+import { useCall, VideoPreview } from '@stream-io/video-react-sdk'
+import React, { useEffect, useState } from 'react'
 
 const MeetingSetup = () => {
+  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false)
+  const call = useCall()
+
+  if(!call) {
+    throw new Error("usecall must be used within streamcall component")
+  }
+
+  useEffect(() => {
+    if(isMicCamToggledOn) {
+        call?.camera.disable()
+        call?.microphone.disable()
+    } else {
+        call?.camera.enable()
+        call?.microphone.enable()
+    }
+  }, [isMicCamToggledOn, call?.camera, call?.microphone])
   return (
-    <div>MeetingSetup</div>
+    <div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
+        <h1 className='text-2xl font-bold'>Setup</h1>
+        <VideoPreview />
+    </div>
   )
 }
 
