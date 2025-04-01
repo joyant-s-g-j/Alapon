7397987171fe481ffad50391546670cc2d9da059
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-04-02 01:18:40 +0600

    toast added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import {ClerkProvider, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from '@clerk/nextjs'
+import {ClerkProvider } from '@clerk/nextjs'
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -43,6 +44,7 @@ export default function RootLayout({
       >
         <body className={`${geistSans.variable} ${geistMono.variable} bg-dark-2 antialiased`}>
           {children}
+          <Toaster />
         </body>
       </ClerkProvider>
     </html>
