f2704569a67102858008f3419aba42b1f07f4bb9
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-04-01 21:05:54 +0600

    card dif color

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -1,10 +1,18 @@
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import React from 'react'
 
-const HomeCard = () => {
+interface HomeCardProps{
+    img: string,
+    title: string,
+    description: string,
+    handleClick: () => void,
+    className: string
+}
+
+const HomeCard = ({img, title, description, handleClick, className}: HomeCardProps) => {
   return (
-    <div className='bg-orange-1 px-4 py-6 flex flex-col justify-between 
-        w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer'
+    <div className={cn('px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer', className)}
         onClick={() => {}}>
         <div className='flex-center glassmorphism size-12 rounded-[10px]'>
             <Image 
