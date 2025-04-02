fe7466b729f6bdd3053485db6a1596dcf43b667d
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-04-03 01:32:57 +0600

    showing participents

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,8 +1,8 @@
 import { cn } from '@/lib/utils'
-import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
+import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
 import React, { useState } from 'react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
-import { LayoutList } from 'lucide-react'
+import { LayoutList, Users } from 'lucide-react'
 
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
 
@@ -25,7 +25,7 @@ const MeetingRoom = () => {
         <div className='flex size-full max-w-[1000px] items-center'>
           <CallLayout />
         </div>
-        <div className={cn('h-[calc(100vh-86px)] hidden ml-2', {'show-block': showParticipants})}>
+        <div className={cn('h-[calc(100vh-86px)] ml-2', {'hidden': !showParticipants,'block': showParticipants})}>
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
@@ -52,6 +52,13 @@ const MeetingRoom = () => {
             ))}
           </DropdownMenuContent>
         </DropdownMenu>
+
+        <CallStatsButton />
+        <button onClick={() => setShowParticipants((prev) => !prev)}>
+          <div className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
+            <Users size={20} className='text-white' />
+          </div>
+        </button>
       </div>
     </section>
   )
