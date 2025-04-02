4369270636f6cb24139d82f82118bec7cbbac4df
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-04-03 00:29:09 +0600

    call join

--- src/components/MeetingSetup.tsx
+++ src/components/MeetingSetup.tsx
@@ -38,7 +38,7 @@ const MeetingSetup = ({setIsSetupComplete} : {
           <DeviceSettings />
         </div>
         <Button 
-          className='rounded-md bg-green-500 px-4 py-2.5'
+          className='rounded-xl hover:bg-green-800 bg-green-500 px-4 py-2.5'
           onClick={() => {
             call.join()
             setIsSetupComplete(true)
