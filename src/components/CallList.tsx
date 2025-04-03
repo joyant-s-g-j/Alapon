a8e3825dfd8ac3ae0f6841834109b0e6a8a40ac5
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-04-04 01:22:29 +0600

    fetch recordings

--- src/components/CallList.tsx
+++ src/components/CallList.tsx
@@ -2,7 +2,7 @@
 import { useGetCalls } from '@/hooks/useGetCalls'
 import { Call, CallRecording } from '@stream-io/video-react-sdk'
 import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import MeetingCard from './MeetingCard'
 import Loader from './Loader'
 
@@ -37,6 +37,20 @@ const CallList = ({type}: {type: 'ended' | 'upcoming' | 'recordings'}) => {
     }
   }
 
+  useEffect(() => {
+    const fetchRecordings = async () => {
+      const callData = await Promise.all(callRecordings.map(
+        (meeting) => meeting.queryRecordings()))
+      
+      const recordings = callData
+        .filter(call => call.recordings.length > 0)
+        .flatMap(call => call.recordings)
+      
+        setRecordings(recordings)
+    }
+    if(type === 'recordings') fetchRecordings()
+  }, [type, callRecordings])
+
   const calls = getCalls();
   const noCallsMessage = getNoCallsMessage()
 
@@ -49,8 +63,8 @@ const CallList = ({type}: {type: 'ended' | 'upcoming' | 'recordings'}) => {
           icon={
             type === 'ended' ? '/icons/previous.svg' : type === 'upcoming' ? '/icons/upcoming.svg' : '/icons/recordings.svg'
           }
-          title={(meeting as Call).state.custom.description.substring(0, 20) || 'No Description'}
-          date={(meeting as Call).state.startsAt?.toLocaleString() || (meeting as CallRecording)?.start_time?.toLocaleString() || 'No Date'}
+          title={(meeting as Call).state?.custom.description.substring(0, 20) || (meeting as CallRecording).filename.substring(0, 20) || 'No Description'}
+          date={(meeting as Call).state?.startsAt?.toLocaleString() || (meeting as CallRecording)?.start_time?.toLocaleString() || 'No Date'}
           isPreviousMeeting={type==='ended'}
           buttonIcon1={type === 'recordings' ? '/icons/play.svg' : undefined}
           buttonText={type === 'recordings' ? 'Play' : 'Start'}
